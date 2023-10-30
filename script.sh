5ea1c2739e268d6f657fda3812c1e6043946b36f
z <z@m2.local>
2023-10-30 14:42:08 -0600

    Add tofu integration tf vars support

--- script.sh
+++ script.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
+## exporting module name
 export MODULE=${1}
+## exporting vars to tf_vars
+shift
+for ARGUMENT in "${@}"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+   export "TF_VAR_$KEY"="$VALUE"
+done
+
 export GITMODULE="https://git.zulu.house/zulunity/$MODULE.git"
 ## acquiring state bucket
 export BUCKET="$(aws s3api list-buckets --query 'Buckets[?starts_with(Name, `zulunity-remote-state`)].{Name:Name, CreationDate:CreationDate}' | jq -r 'sort_by(.CreationDate)|reverse|.[0].Name')"
@@ -8,14 +20,14 @@ export AWS_ACCOUNT_ID="$(aws sts get-caller-identity  | jq -r .Account)"
 ## echo summary
 echo "========== Script to install AWS $MODULE on $AWS_ACCOUNT_ID ========"
 ## Asking for required packages
-if [ -z `which terraform` ]
+if [ -z `which tofu` ]
 then
-    # Install yum-utils
-    sudo yum install -y yum-utils
-    # Use yum-config-manager to add the official HashiCorp Linux repository.
-    sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-    # Install terraform 
-    sudo yum -y install terraform 1.5.6-1
+    # Install tofu
+    wget https://github.com/opentofu/opentofu/releases/download/v1.6.0-alpha3/tofu_1.6.0-alpha3_linux_amd64.zip
+    unzip tofu_1.6.0-alpha3_linux_amd64.zip
+    chmod +x tofu
+    sudo mv tofu /usr/local/bin/tofu
+
 fi
 
 # Clone MODULE
@@ -26,8 +38,9 @@ cd $MODULE
 echo -en 'terraform {\n  backend "s3" {}\n}' > backend.tf
 export TF_VAR_account_id="$AWS_ACCOUNT_ID"
 export TF_VAR_description="AWS $MODULE"
-terraform init \
+export TF_VAR_region="$AWS_REGION"
+tofu init \
     -backend-config="bucket=$BUCKET" \
     -backend-config="key=state" \
     -backend-config="region=$AWS_REGION"
-terraform apply -auto-approve
+tofu apply -auto-approve
